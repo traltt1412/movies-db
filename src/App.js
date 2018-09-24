7a4ab63cb85392189089bfe69f7d46186441fae6
ThanhTra Le <thanhtra.le@ogilvy.com>
2018-09-24 14:09:04 +0700

    add sass loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Header from './components/Header/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header></Header>
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Header.scss'
+
+export default class Header extends React.Component {
+  render () {
+    return <div>This is header</div>
+  }
+}
